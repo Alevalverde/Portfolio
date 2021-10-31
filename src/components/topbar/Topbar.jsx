11561e800d727246a64fcb5189aa9cb67ab9d616
Alevalverde <alevalverdev8@hotmail.com>
2021-10-30 23:08:17 -0300

    sections flow finish

--- src/components/topbar/Topbar.jsx
+++ src/components/topbar/Topbar.jsx
@@ -1,18 +1,43 @@
-import "./topbar.scss"
+import { Mail, WhatsApp, GitHub, LinkedIn } from "@mui/icons-material";
+import "./topbar.scss";
 
-export default function Topbar() {
-    return (
-        <div className="topbar">
-            <div className="wrapper">
-                <div className="left">
-                    <a href="#intro">Intro</a>
-                    <a href="#portfolio">Portfolio</a>
-                    <a href="#works">Works</a>
-                    <a href="#testimonials">Testimonials</a>
-                    <a href="#contact">Contact</a>
-                </div>
-            </div>
-            
+export default function Topbar({menuOpen, setMenuOpen}) {
+  
+  
+  return (
+    <div className={"topbar " + (menuOpen && "active")}>
+      <div className="wrapper">
+        <div className="left">
+          <div className="itemContainer">
+            <LinkedIn className="icon" />
+            {/* <span>+549 261-206281</span> */}
+          </div>
+          <div className="itemContainer">
+            <WhatsApp className="icon" />
+            {/* <span>+549 261-206281</span> */}
+          </div>
+          <div className="itemContainer">
+            <GitHub className="icon" />
+            {/* <span>Alevalverde</span> */}
+          </div>
+          <div className="itemContainer">
+            <Mail className="icon" />
+            {/* <span>alevalverdev8@gmail.com</span> */}
+          </div>
         </div>
-    )
+        <div className="center">
+          <a href="#intro" className="logo">
+            αλέξανδρος. 
+          </a>
+        </div>
+        <div className="right">
+          <div className="hamburger" onClick={()=>setMenuOpen(!menuOpen)}>
+            <span className="line1"></span>
+            <span className="line2"></span>
+            <span className="line3"></span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
